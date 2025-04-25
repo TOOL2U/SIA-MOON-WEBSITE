e06e78380d727f76542a2e26ee9371dbb2db43a8
Shaun github <shaunducker1@gmail.com>
2025-04-25 17:23:05 +0700

    Fix server-side rendering issues for Vercel deployment

--- app/utils/icons.tsx
+++ app/utils/icons.tsx
@@ -1,43 +1,45 @@
 // This file provides a safe way to import icons from react-icons
 // It handles the CommonJS/ESM compatibility issues that can occur on the server
 
-// Import directly from the specific icon files to avoid ESM/CommonJS compatibility issues
-import { FaTimes } from 'react-icons/fa';
-import { FaBars } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaPinterest } from 'react-icons/fa';
-import { FaCalendarAlt } from 'react-icons/fa';
-import { FaUser } from 'react-icons/fa';
-import { FaEnvelope } from 'react-icons/fa';
-import { FaPhone } from 'react-icons/fa';
-import { FaGlobe } from 'react-icons/fa';
-import { FaUsers } from 'react-icons/fa';
-import { FaTrash } from 'react-icons/fa';
-import { FaPlus } from 'react-icons/fa';
-import { FaBan } from 'react-icons/fa';
-import { FaSignOutAlt } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
-import { FaEdit } from 'react-icons/fa';
-import { FaCheck } from 'react-icons/fa';
-import { FaMapMarkerAlt } from 'react-icons/fa';
-import { FaBed } from 'react-icons/fa';
-import { FaBath } from 'react-icons/fa';
-import { FaRulerCombined } from 'react-icons/fa';
-import { FaDirections } from 'react-icons/fa';
-import { FaMapPin } from 'react-icons/fa';
-import { FaLock } from 'react-icons/fa';
-import { FaTicketAlt } from 'react-icons/fa';
-import { FaCopy } from 'react-icons/fa';
-import { FaExclamationTriangle } from 'react-icons/fa';
-import { FaArrowRight } from 'react-icons/fa';
-import { FaChevronLeft } from 'react-icons/fa';
-import { FaChevronRight } from 'react-icons/fa';
-import { FaArrowLeft } from 'react-icons/fa';
+// Import all FA icons in a single import statement
+import {
+  FaTimes,
+  FaBars,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaPinterest,
+  FaCalendarAlt,
+  FaUser,
+  FaEnvelope,
+  FaPhone,
+  FaGlobe,
+  FaUsers,
+  FaTrash,
+  FaPlus,
+  FaBan,
+  FaSignOutAlt,
+  FaSearch,
+  FaEdit,
+  FaCheck,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaDirections,
+  FaMapPin,
+  FaLock,
+  FaTicketAlt,
+  FaCopy,
+  FaExclamationTriangle,
+  FaArrowRight,
+  FaChevronLeft,
+  FaChevronRight,
+  FaArrowLeft
+} from 'react-icons/fa';
 
-import { SlArrowRight } from 'react-icons/sl';
-import { SlArrowLeft } from 'react-icons/sl';
+// Import all SL icons in a single import statement
+import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
 
 // Re-export all icons
 export {
