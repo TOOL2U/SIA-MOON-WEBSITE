49f32aa495d2573126b493595bb4def01374fc9c
Shaun github <shaunducker1@gmail.com>
2025-04-25 17:18:40 +0700

    Fix server-side rendering issues for Vercel deployment

--- app/utils/icons.tsx
+++ app/utils/icons.tsx
@@ -1,42 +1,79 @@
 // This file provides a safe way to import icons from react-icons
 // It handles the CommonJS/ESM compatibility issues that can occur on the server
 
-import * as FaIcons from 'react-icons/fa';
-import * as SlIcons from 'react-icons/sl';
+// Import directly from the specific icon files to avoid ESM/CommonJS compatibility issues
+import { FaTimes } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaPinterest } from 'react-icons/fa';
+import { FaCalendarAlt } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
+import { FaGlobe } from 'react-icons/fa';
+import { FaUsers } from 'react-icons/fa';
+import { FaTrash } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { FaBan } from 'react-icons/fa';
+import { FaSignOutAlt } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import { FaEdit } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { FaBed } from 'react-icons/fa';
+import { FaBath } from 'react-icons/fa';
+import { FaRulerCombined } from 'react-icons/fa';
+import { FaDirections } from 'react-icons/fa';
+import { FaMapPin } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
+import { FaTicketAlt } from 'react-icons/fa';
+import { FaCopy } from 'react-icons/fa';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronRight } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
 
-export const FaTimes = FaIcons.FaTimes;
-export const FaBars = FaIcons.FaBars;
-export const FaFacebook = FaIcons.FaFacebook;
-export const FaInstagram = FaIcons.FaInstagram;
-export const FaTwitter = FaIcons.FaTwitter;
-export const FaPinterest = FaIcons.FaPinterest;
-export const FaCalendarAlt = FaIcons.FaCalendarAlt;
-export const FaUser = FaIcons.FaUser;
-export const FaEnvelope = FaIcons.FaEnvelope;
-export const FaPhone = FaIcons.FaPhone;
-export const FaGlobe = FaIcons.FaGlobe;
-export const FaUsers = FaIcons.FaUsers;
-export const FaTrash = FaIcons.FaTrash;
-export const FaPlus = FaIcons.FaPlus;
-export const FaBan = FaIcons.FaBan;
-export const FaSignOutAlt = FaIcons.FaSignOutAlt;
-export const FaSearch = FaIcons.FaSearch;
-export const FaEdit = FaIcons.FaEdit;
-export const FaCheck = FaIcons.FaCheck;
-export const FaMapMarkerAlt = FaIcons.FaMapMarkerAlt;
-export const FaBed = FaIcons.FaBed;
-export const FaBath = FaIcons.FaBath;
-export const FaRulerCombined = FaIcons.FaRulerCombined;
-export const FaDirections = FaIcons.FaDirections;
-export const FaMapPin = FaIcons.FaMapPin;
-export const FaLock = FaIcons.FaLock;
-export const FaTicketAlt = FaIcons.FaTicketAlt;
-export const FaCopy = FaIcons.FaCopy;
-export const FaExclamationTriangle = FaIcons.FaExclamationTriangle;
-export const FaArrowRight = FaIcons.FaArrowRight;
-export const FaChevronLeft = FaIcons.FaChevronLeft;
-export const FaChevronRight = FaIcons.FaChevronRight;
-export const FaArrowLeft = FaIcons.FaArrowLeft;
+import { SlArrowRight } from 'react-icons/sl';
+import { SlArrowLeft } from 'react-icons/sl';
 
-export const SlArrowRight = SlIcons.SlArrowRight;
-export const SlArrowLeft = SlIcons.SlArrowLeft;
+// Re-export all icons
+export {
+  FaTimes,
+  FaBars,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaPinterest,
+  FaCalendarAlt,
+  FaUser,
+  FaEnvelope,
+  FaPhone,
+  FaGlobe,
+  FaUsers,
+  FaTrash,
+  FaPlus,
+  FaBan,
+  FaSignOutAlt,
+  FaSearch,
+  FaEdit,
+  FaCheck,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaDirections,
+  FaMapPin,
+  FaLock,
+  FaTicketAlt,
+  FaCopy,
+  FaExclamationTriangle,
+  FaArrowRight,
+  FaChevronLeft,
+  FaChevronRight,
+  FaArrowLeft,
+  SlArrowRight,
+  SlArrowLeft
+};
